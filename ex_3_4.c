fa3bda27281b0794dca8374137e982ff5f8305b0
david <seaspring@qq.com>
2012-05-21 20:43:49 +0800

    add code

---
+++ ex_3_4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+int main(void){
+   pid_t childpid ;
+   childpid = fork();
+   if (childpid == -1){
+  	perror("Failed to fork");
+        return 1;
+    }
+   if (childpid == 0) {
+      execl("/bin/ls","ls","-l",NULL);
+      perror("Child failed to exec ls");
+      return 1;
+   }
+   if (childpid != wait(NULL)) {
+      perror("Parent failed to wait due to signal or error");
+      return 1;
+   }
+   return 0;
+}
+
